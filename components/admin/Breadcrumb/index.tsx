a3c278c33d0f4a5643bb7fa2700a9a621b9d10bf
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2024-09-21 14:55:33 +0700

    added: Breadcrumb admin component

--- components/admin/Breadcrumb/index.tsx
+++ components/admin/Breadcrumb/index.tsx
@@ -1,18 +1,18 @@
 import { Breadcrumb } from "antd";
 import Link from "next/link";
 import React from "react";
-export type Breadcrumb = {
+
+export type BreadcrumbItem = {
   url: string;
   title: string;
 };
-export default function Index({ data }: { data: Breadcrumb[] }) {
-  return (
-    <Breadcrumb style={{ margin: "16px 0" }}>
-      {data.map((item: Breadcrumb) => (
-        <Breadcrumb.Item key={item.url}>
-          <Link href={item.url}>{item.title}</Link>
-        </Breadcrumb.Item>
-      ))}
-    </Breadcrumb>
-  );
+
+export default function Index({ data }: { data: BreadcrumbItem[] }) {
+  // Map your breadcrumb data to the structure expected by the `items` prop
+  const breadcrumbItems = data.map((item) => ({
+    key: item.url,
+    title: <Link href={item.url}>{item.title}</Link>,
+  }));
+
+  return <Breadcrumb style={{ margin: "16px 0" }} items={breadcrumbItems} />;
 }
