4a0c1f6664a964c5c1b6ecde0bc27c4e6967ae26
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2024-09-21 14:57:50 +0700

    added: interface IPost nd IUser

--- types/api.d.ts
+++ types/api.d.ts
@@ -3,3 +3,22 @@ interface Response<T> {
   succeeded: boolean;
   data: T;
 }
+interface IPost {
+  description: string;
+  content: string;
+  slug: string;
+  thumbnail: string;
+}
+
+interface IUser {
+  id: string;
+  phoneNumber: string;
+  email: string;
+  userName: string;
+  accessFailedCount: number;
+  emailConfirmed: boolean;
+  phoneNumberConfirmed: boolean;
+  fullName: string;
+  createdAt: string;
+  logo: string;
+}
